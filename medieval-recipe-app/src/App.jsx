9653dd167a5a3aa22992f7576a2b68a2401559ed
Gabriel <gabrielcoxomega@gmail.com>
2024-04-04 18:15:18 +0100

    feat: replace output text area with a div masquerading as a text area

--- medieval-recipe-app/src/App.jsx
+++ medieval-recipe-app/src/App.jsx
@@ -3,18 +3,42 @@ import Textarea from '@mui/joy/Textarea';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
-import { useState } from 'react';
+import { useEffect, useRef, useState, useLayoutEffect } from 'react';
 import './App.css';
 
 import Translator from './translation/Translator';
 
 function App() {
+  // Source is the input text to be translated
   const [sourceText, setSourceText] = useState('');
-  const [targetText, setTargetText] = useState('');
+  const [translation, setTranslation] = useState('');
+
+  /* The output code is in a div masquerading as a text area, and this 
+   * code ensures it matches the height of actual text area used as the 
+   * input, and copies its className attribute. */
+  const sourceTextAreaMinRows = 4;
+  const sourceGridRef = useRef(null);
+  const sourceTextAreaRef = useRef(null);
+  const [textAreaHeight, setTextAreaHeight] = useState(0);
+  const [textAreaClassName, setTextAreaClassName] = useState('');
+
+  useLayoutEffect(() => {
+    setTextAreaHeight(sourceGridRef.current.clientHeight)
+  }, [sourceText]);
+
+  useEffect(() => {
+    window.addEventListener('resize',
+      () => setTextAreaHeight(sourceGridRef.current.clientHeight));
+  }, [])
+
+  useEffect(() => {
+    setTextAreaClassName(sourceTextAreaRef.current.className);
+  }, [])
+  /* End of code to help a div masquerade as a text area */
 
   const handleTranslate = async () => {
     const translatedText = Translator.translateText(sourceText);
-    setTargetText(translatedText);
+    setTranslation(translatedText);
   };
 
   const bibliography = [
@@ -24,9 +48,6 @@ function App() {
     { title: "Two Fifteenth-Century Cookery-Books. Harleian MS. 279 & Harl. MS. 4016, with extracts from Ashmole MS. 1429, Laud MS. 553, & Douce MS 55", author: "Thomas Austin", year: "1888" },
   ];
 
-  const minRows = 4;
-  const maxRows = 10;
-
   return (
     <div className="App">
       <div id="AppBody">
@@ -34,30 +55,23 @@ function App() {
         <p>This website offers a simple, word-for-word translation of medieval English recipes into more modern English. It is by no means perfect, and is intended to help budding enthusiasts get familiar with some of the common yet esoteric terms that appear in period manuscripts.</p>
         <p>Translated recipes will still require significant interpretation in most cases. For more beginner-friendly recipes online, I suggest websites like <a href="http://www.godecookery.com/">Gode Cookery</a> and <a href="https://medievalcookery.com/index.html">Medieval Cookery</a>.</p>
         <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2} alignItems="center">
-            <Grid item xs={12} sm={6}>
+          <Grid container spacing={2} alignItems="flex-start">
+            <Grid item xs={12} sm={6} ref={sourceGridRef} >
               <Textarea
+                ref={sourceTextAreaRef}
                 color="primary"
                 label="Enter text"
                 value={sourceText}
                 onChange={(e) => setSourceText(e.target.value)}
-                minRows={minRows}
-                maxRows={maxRows}
+                minRows={sourceTextAreaMinRows}
                 variant="outlined"
                 placeholder="Enter recipe here..."
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Textarea
-                color="primary"
-                label="Translated text"
-                value={targetText}
-                readOnly
-                minRows={minRows}
-                maxRows={maxRows}
-                variant="outlined"
-                placeholder="Translation appears here"
-              />
+            <Grid item xs={12} sm={6} style={{ minHeight: `${textAreaHeight}px`, display: 'flex' }}>
+              <div style={{ flexGrow: '1' }} className={textAreaClassName} >
+                {translation}
+              </div>
             </Grid>
             <Grid item>
               <Button
