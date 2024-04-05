6ea03ebc89864bf6cc9cd4196291295125b1a885
Gabriel <gabrielcoxomega@gmail.com>
2024-04-05 22:48:53 +0100

    chore: add **/*.jsonl to vite config assetsInclude

--- medieval-recipe-app/vite.config.js
+++ medieval-recipe-app/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://gjcox.github.io/MedievalRecipeTranslatorApp/"
+  base: "https://gjcox.github.io/MedievalRecipeTranslatorApp/",
+  assetsInclude: ['**/*.jsonl'],
 })
