25c6b9d3b1d9cf23862843e3255ddf16522a1771
Gabriel <gabrielcoxomega@gmail.com>
2024-05-29 16:50:35 +0100

    test: add GlossaryEntry unit tests

---
+++ medieval-recipe-app/tests/GlossaryEntry.test.jsx
@@ -0,0 +1,40 @@
+import { fireEvent, screen, render } from '@testing-library/react';
+import { assert, describe, expect, it, vi } from 'vitest';
+
+import GlossaryEntry, { PLACEHOLDER_TEXT } from '../src/translation/GlossaryEntry';
+
+const onClick = vi.fn();
+
+describe('GlossaryEntry', () => {
+    it('should have placeholder text if there are no substitutions', () => {
+        render(<GlossaryEntry
+            word={"testeth"}
+            meanings={[]}
+            setSubstitution={onClick}
+        />);
+
+        screen.getByText(PLACEHOLDER_TEXT);
+    });
+    it('should not have placeholder text if there are substitutions', () => {
+        render(<GlossaryEntry
+            word={"testeth"}
+            meanings={[{ substitution: "test", note: "very important in software developement" }]}
+            setSubstitution={onClick}
+        />);
+
+        const placeholder = screen.queryByText(PLACEHOLDER_TEXT);
+        assert.isNull(placeholder, "there should not be placeholder text");
+    });
+    it('should call "setSubstitution" when a substitution is clicked', () => {
+        render(<GlossaryEntry
+            word={"testeth"}
+            meanings={[{ substitution: "test", note: "very important in software developement" }]}
+            setSubstitution={onClick}
+        />);
+        const links = screen.getAllByRole("link");
+
+        fireEvent.click(links[0])
+
+        expect(onClick).toBeCalledTimes(1);
+    });
+});
