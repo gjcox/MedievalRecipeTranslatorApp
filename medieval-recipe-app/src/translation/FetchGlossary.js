c94f7b10a02b006df629a62a35d6a628014d0120
Gabriel <gabrielcoxomega@gmail.com>
2024-05-30 15:28:27 +0100

    feat!: remove BuildGlossary and FetchGlossary so they can be renamed properly

--- medieval-recipe-app/src/translation/FetchGlossary.js
+++
@@ -1,8 +0,0 @@
-export default async function fetchGlossary() {
-    const URL = import.meta.env.BASE_URL + 'glossary.jsonl';
-    const response = await fetch(URL);
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.text();
-}
